e68ad8aa83f93f9ba7a53680e96f2b5f0f28ca2a
harshbuildsstuff <harshch2208@gmail.com>
2026-03-22 21:09:01 +0000

    config: initial config with headers and retry settings

---
+++ scraper/config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://josaa.nic.in/"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+        "Accept-Language": "en-US,en;q=0.9"
+        }
+
+        RETRY_COUNT = 3
+        TIMEOUT = 10
