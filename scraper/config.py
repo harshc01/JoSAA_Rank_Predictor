b76a2f85519be7269fb743dfe568312d445b46f1
Harsh Chauhan <88181081+harshbuildsstuff@users.noreply.github.com>
2026-03-23 04:14:03 +0530

    indentation fix

--- scraper/config.py
+++ scraper/config.py
@@ -2,11 +2,12 @@ BASE_URL = "https://josaa.nic.in/"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9"
-        }
+    "Accept-Language": "en-US,en;q=0.9"
+}
 
-        RETRY_COUNT = 3
-        TIMEOUT = 10
+RETRY_COUNT = 3
+TIMEOUT = 10
+
 DROPDOWNS = [
     "ctl00$ContentPlaceHolder1$ddlYear",
     "ctl00$ContentPlaceHolder1$ddlRoundNo",
